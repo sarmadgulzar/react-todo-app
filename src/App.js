5b3b3622992b12469e12eb1ec14007620fd68ff6
Sarmad Gulzar <sarmadgulzar@hotmail.com>
2020-10-29 06:46:37 +0500

    Added TodoItem component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import React from "react";
 
+import TodoItem from "./TodoItem";
+
 function App() {
   return (
     <div>
-      <input type="checkbox" />
-      <p>Placeholder text here</p>
-
-      <input type="checkbox" />
-      <p>Placeholder text here</p>
-
-      <input type="checkbox" />
-      <p>Placeholder text here</p>
-
-      <input type="checkbox" />
-      <p>Placeholder text here</p>
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
     </div>
   );
 }
