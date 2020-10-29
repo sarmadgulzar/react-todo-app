4394858a6399d62a5ec8c6d8faf213a9af206145
Sarmad Gulzar <sarmadgulzar@hotmail.com>
2020-10-29 23:04:35 +0500

    Added state to App component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,20 @@ import React from "react";
 import TodoItem from "./TodoItem";
 import todosData from "./todosData";
 
-function App() {
-  const todoItems = todosData.map((item) => (
-    <TodoItem key={item.id} item={item} />
-  ));
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: todosData,
+    };
+  }
+  render() {
+    const todoItems = this.state.todos.map((item) => (
+      <TodoItem key={item.id} item={item} />
+    ));
 
-  return <div className="todo-list">{todoItems}</div>;
+    return <div className="todo-list">{todoItems}</div>;
+  }
 }
 
 export default App;
