015e234e58275535033e7fc345021f1167279049
Sarmad Gulzar <sarmadgulzar@hotmail.com>
2020-10-29 23:24:21 +0500

    Added Checkbox state

--- src/App.js
+++ src/App.js
@@ -9,10 +9,22 @@ class App extends React.Component {
     this.state = {
       todos: todosData,
     };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(id) {
+    this.setState((prevState) => {
+      const updatedTodos = prevState.todos.map((todo) => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      });
+      return { todo: updatedTodos };
+    });
   }
   render() {
     const todoItems = this.state.todos.map((item) => (
-      <TodoItem key={item.id} item={item} />
+      <TodoItem key={item.id} item={item} handleChange={this.handleChange} />
     ));
 
     return <div className="todo-list">{todoItems}</div>;
