e40d9c68b58e7b2fd2931d5257e2d5d178004d64
Sarmad Gulzar <sarmadgulzar@hotmail.com>
2020-10-29 07:50:02 +0500

    Rendering TodoItem components based on todosData

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from "react";
 
 import TodoItem from "./TodoItem";
+import todosData from "./todosData";
 
 function App() {
-  return (
-    <div className="todo-list">
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
-    </div>
-  );
+  const todoItems = todosData.map((item) => (
+    <TodoItem key={item.id} item={item} />
+  ));
+
+  return <div className="todo-list">{todoItems}</div>;
 }
 
 export default App;
